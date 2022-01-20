ca227dc1224dd25cea8591ea9b4bfe16081c3bc1
Namhoon-95 <jnh9575@naver.com>
2022-01-20 19:41:58 +0900

    ### feat : About 페이지 완성

--- frontend/src/component/Navigation.js
+++ frontend/src/component/Navigation.js
@@ -5,19 +5,36 @@ import Board from '../veiw/Board';
 import Login from '../veiw/Login';
 import MyPage from '../veiw/MyPage';
 import logo from '../assets/img/CookubLogo.png'
-import './Navigation.css';
+import style from './Navigation.module.css';
+import { Button } from 'react-bootstrap';
 
 
 
 function Navigation() {
   return ( 
-    <div className='nav'>
-      <img className='logo_img' src={logo} alt="COOKUB" title="Cookub"/> 
-      <Link to='/login' element={<Login/>}>LOGIN</Link>
-      <Link to='/about' element={<About/>}>ABOUT</Link>
-      <Link to='/board' element={<Board/>}>PUBLIC RECIPE</Link>
-      <Link to='/mypage' element={<MyPage/>}>REPOSITORY</Link>
+    <div>
+      <img className={style.logo_img} src={logo} alt="COOKUB" /> 
+      <button className={style.login_button}>LOGIN</button><br/>
+      <li className={style.list_item}><Link className={style.nav_item} to='/about' element={<About/>}>ABOUT</Link></li>
+      <li className={style.list_item}><Link className={style.nav_item} to='/board' element={<Board/>}>PUBLIC RECIPE</Link></li>
+      <li className={style.list_item}><Link className={style.nav_item} to='/mypage' element={<MyPage/>}>REPOSITORY</Link></li>
+      <br/><br/><br/><br/>
+      <p className={style.ceo_pm}>대표번호 :  02 - 9575 - 4323</p>
+      <p className={style.footer}>
+        주식회사  레인보우<br/>
+        owner 김남현<br/>
+        서울특별시 금천구 가산디지털1로(가산동)<br/>
+        우림라이온스밸리 8층<br/>
+        budiness licence 740-99-1053<br/>
+        online business licence 2022-서울서초구-1532<br/>
+        개인정보 관리 책임자 : 레인보우(skagns@gmail.com)<br/><br/>
+
+        이용약관 <br/>
+        개인정보취급방침<br/>
+        이용안내<br/>
+      </p>
     </div>
+
    );
 }
 
