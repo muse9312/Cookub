6f0554810485910c3ebfde435e440c73a55500a4
commGom <hyunwoo921129@gmail.com>
2022-01-26 20:28:47 +0900

    ### fix : TestController 코드 작성

--- backend/src/main/java/com/cookub/backend/controller/TestController.java
+++ backend/src/main/java/com/cookub/backend/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.cookub.backend.controller;
 
+import com.cookub.backend.dto.RecipeDto;
 import com.cookub.backend.entity.Recipe;
 import com.cookub.backend.util.Response;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
-    @PostMapping("/")
-    public Response registerRecipe(@RequestBody Recipe recipe){
+    @PostMapping("/recipe")
+    public Response registerRecipe(@RequestBody RecipeDto recipe){
         System.out.println(recipe);
         Response response = new Response();
         response.add("recipe",recipe);
