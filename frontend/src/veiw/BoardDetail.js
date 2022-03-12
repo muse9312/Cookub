cd88afb04a0b2c689f459a5c348cb2603bde69df
Namhoon-95 <jnh9575@naver.com>
2022-03-12 13:48:07 +0900

    ###feat: recipe CR

--- frontend/src/veiw/BoardDetail.js
+++ frontend/src/veiw/BoardDetail.js
@@ -1,17 +1,156 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import QRcode from 'qrcode.react'
+import axios from 'axios';
+// import { style } from '@mui/system';
+import style from './BoardDetail.module.css'
+import img from '../assets/img/testfood.jpg';
+import{TiEdit,TiLockClosed,TiPuzzle,TiStar, TiStarFullOutline, TiStopwatch, TiTag} from 'react-icons/ti';
 
 function BoardDetail() {
+
+  const[recipe, setRecipe] = useState([]);
+
+  useEffect(()=>{
+    const id = window.sessionStorage.getItem("detail_recipeId")
+    axios(
+      {
+        url:`http://localhost:8080/mypage/recipe/${id}`,
+        method:'get'
+      }
+    ).then((res)=>{
+      console.log(res);
+      console.log(res.data);
+      setRecipe(res.data);
+    })
+  },[]);
+
   return (
-    <div>
-      <h1> 공유 레시피의 상세페이지 입니다.</h1>
-      {/* <QRcode 
-                        id="myqr"
-                        value={"https://github.com/Namhoon-95"} 
-                        size={320}
-                        includeMargin={true}
-                    /> */}
+    /* <QRcode id="myqr" value={"https://github.com/Namhoon-95"} 
+            size={320} includeMargin={true} /> --> QR 코드 생성코드 */
+
+  <div >
+    <div className={style.container}>
+      <div className={style.empty}/>
+      <div className={style.container2}>
+        {/* 제목 */}
+        <h1 className={style.title}>
+          {recipe.title==null?"제목이 없습니다.":recipe.title}
+          </h1>
+        <div className={style.step_top}>
+          <div className={style.top_cont}>
+            <div className={style.top_img}>
+              <img className={style.img} src={img} alt="testimg" title="testimg" />
+            </div>
+            <div className={style.top_discription}>
+              {/* 난이도, 키포인트, 공개여부, 조리시간, 키워드 */}
+              <ul className={style.disc_list}>
+                <li className={style.disc_item}>
+                  <div className={style.disc_icon}><TiStarFullOutline/></div>
+                  <span className={style.disc_text}>키포인트</span>
+                  <p className={style.disc_text2}>
+                    {recipe.keypoint==null?"내용이 없습니다.":recipe.keypoint}
+                  </p>
+                </li>
+                <li className={style.disc_item}>
+                  <div className={style.disc_icon}><TiLockClosed/></div>
+                  <span className={style.disc_text}>공개여부</span>
+                  <span className={style.disc_text2}>
+                    {recipe.isOpenable==null?
+                    "내용이 없습니다.":
+                    recipe.isOpenable===1?"공개":"비공개"}
+                  </span>
+                </li>
+                <li className={style.disc_item}>
+                  <div className={style.disc_icon}><TiPuzzle/></div>
+                  <span className={style.disc_text}>난이도</span>
+                  <span className={style.disc_text2}>
+                    &nbsp;&nbsp;&nbsp;{recipe.level==null?"내용이 없습니다.":recipe.level}
+                  </span>
+                </li>
+                <li className={style.disc_item}>
+                  <div className={style.disc_icon}><TiStopwatch/></div>
+                  <span className={style.disc_text}>조리시간</span>
+                  <span className={style.disc_text2}>
+                    {recipe.cookingTime==null?"내용이 없습니다.":recipe.cookingTime}&nbsp;분
+                  </span>
+                </li>
+                <li className={style.disc_item}>
+                  <div className={style.disc_icon}><TiTag/></div>
+                  <span className={style.disc_text}>키워드</span>
+                  <span className={style.disc_text2}>
+                    {recipe.keywordList==null?'':recipe.keywordList.map((v)=>("#"+v.keywordName +"  "))}
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        {/* 아래 코드는 재료목록 코드 */}
+        <div className={style.ingre_cont}>
+          <div className={style.ingreTitle}>
+            <b className={style.ingreT1}>재료</b>
+            <span className={style.ingreT2}>ingredients</span>
+          </div>
+          <div className={style.ingreBox}>
+            <ul className={style.ingreList}>
+              { recipe.ingredients==null?''
+                :recipe.ingredients.map((v)=>(
+                  <li className={style.ingre}>{v.ingredientName}
+                    <span className={style.gram}>{v.amount+"  g"}</span>
+                  </li>
+                ))
+              }
+              
+              {/* <li className={style.ingre}>고구마
+                <span className={style.gram}>30g</span>
+              </li>
+              <li className={style.ingre}>무
+                <span className={style.gram}>50g</span>
+              </li>
+              <li className={style.ingre}>연근
+                <span className={style.gram}>150g</span>
+              </li>
+              <li className={style.ingre}>마
+                <span className={style.gram}>60g</span>
+              </li>
+              <li className={style.ingre}>마늘
+                <span className={style.gram}>80g</span>
+              </li>
+              <li className={style.ingre}>양파
+                <span className={style.gram}>70g</span>
+              </li>
+              <li className={style.ingre}>칡뿌리
+                <span className={style.gram}>300g</span>
+              </li> */}
+            </ul>
+          </div>
+        </div>
+        
+        {/* 아래 코드는 조리법 목록 코드 */}
+        <div className={style.step_cont}>
+          <div className={style.step_title}>
+            <b className={style.step_T1}>조리순서</b>
+            <span className={style.step_T2}>steps</span>
+          </div>
+          {   recipe.ingredients==null?''
+              :recipe.cookMethods.map((v)=>(
+              <div className={style.step_list}>
+                <span className={style.numbering}>{v.step}</span>
+                <div className={style.step_text}>{v.description}</div>
+                <div className={style.step_img}>
+                  <img className={style.img} src={img} alt="testimg" title="testimg" />
+                </div>
+              </div>
+            ))
+          } 
+        </div>
+      </div>
+      <div className={style.empty2}/>
     </div>
+    
+  </div>
+    
   );
 }
 
--- frontend/src/veiw/MyPage.js
+++ frontend/src/veiw/MyPage.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from './MyPage.module.css';
 import Modal from './Modal.js'
 import { Link } from "react-router-dom";
 import img from '../assets/img/testfood.jpg';
-
-
+import axios from "axios";
 
 
 function MyPage() {
@@ -22,8 +21,25 @@ function MyPage() {
 }
 
 function CreateRecipe({ closeModal }) {
+
+  const [dataTest, setDataTest] = useState([]);
+
+  useEffect(()=>{
+    const data = axios(
+      {
+        url:'http://localhost:8080/mypage/recipe/list/2',
+        method:'get'
+      }
+    ).then((res)=>{
+      console.log(res);
+      setDataTest(res.data)})
+
+  },[]);
+
+
+
   return (
-    <div>
+    <div>{/*상단에 새로운레시피 작성 버튼 */}
       <section className={style.container1}>
         <div className={style.My_recipes}>
           <h1 className={style.title}>새로운 레시피 작성을 하시겠습니까?</h1>
@@ -35,91 +51,35 @@ function CreateRecipe({ closeModal }) {
         </div>
       </section>
       
-      {/* 아래는 하드코딩처리된 레시피 카드들 */}
+  
       <section className={style.container2}>
         <div className={style.public_recipes}>
-          <div className={style.recipe}>
-            <Link to="/board/detail" >
-              <img className={style.recipe_img} src={img} alt="testimg" title="testimg" />
-              <h2 className={style.recipe_title}>랍스터 카르파치오</h2>
-              <h5 className={style.recipe_title2}>Lobster Carpaccio</h5>
-              <ul className={style.recipe_genres}>Italia | Lobster | Finger Lime</ul>
-              <p className={style.recipe_summary}>
-                랍스터의 껍질을 제거한 뒤 오렌지, 다임, 소금, 후추를 넣고
-                진공한 뒤 수비드 56도씨에서 2시간 돌린다 비스큐소스, 오렌지주스,
-                화이트와인비...
-              </p>
-            </Link>
-          </div>
-          <div className={style.recipe}>
-            <Link to="/board/detail" >
-              <img className={style.recipe_img} src={img} alt="testimg" title="testimg" />
-              <h2 className={style.recipe_title}>랍스터 카르파치오</h2>
-              <h5 className={style.recipe_title2}>Lobster Carpaccio</h5>
-              <ul className={style.recipe_genres}>Italia | Lobster | Finger Lime</ul>
-              <p className={style.recipe_summary}>
-                랍스터의 껍질을 제거한 뒤 오렌지, 다임, 소금, 후추를 넣고
-                진공한 뒤 수비드 56도씨에서 2시간 돌린다 비스큐소스, 오렌지주스,
-                화이트와인비...
-              </p>
-            </Link>
-          </div>
-
-          <div className={style.recipe}>
-            <Link to="/board/detail" >
-              <img className={style.recipe_img} src={img} alt="testimg" title="testimg" />
-              <h2 className={style.recipe_title}>랍스터 카르파치오</h2>
-              <h5 className={style.recipe_title2}>Lobster Carpaccio</h5>
-              <ul className={style.recipe_genres}>Italia | Lobster | Finger Lime</ul>
-              <p className={style.recipe_summary}>
-                랍스터의 껍질을 제거한 뒤 오렌지, 다임, 소금, 후추를 넣고
-                진공한 뒤 수비드 56도씨에서 2시간 돌린다 비스큐소스, 오렌지주스,
-                화이트와인비...
-              </p>
-            </Link>
-          </div>
-
-          <div className={style.recipe}>
-            <Link to="/board/detail" >
-              <img className={style.recipe_img} src={img} alt="testimg" title="testimg" />
-              <h2 className={style.recipe_title}>랍스터 카르파치오</h2>
-              <h5 className={style.recipe_title2}>Lobster Carpaccio</h5>
-              <ul className={style.recipe_genres}>Italia | Lobster | Finger Lime</ul>
-              <p className={style.recipe_summary}>
-                랍스터의 껍질을 제거한 뒤 오렌지, 다임, 소금, 후추를 넣고
-                진공한 뒤 수비드 56도씨에서 2시간 돌린다 비스큐소스, 오렌지주스,
-                화이트와인비...
-              </p>
-            </Link>
-          </div>
-
-          <div className={style.recipe}>
-            <Link to="/board/detail" >
-              <img className={style.recipe_img} src={img} alt="testimg" title="testimg" />
-              <h2 className={style.recipe_title}>랍스터 카르파치오</h2>
-              <h5 className={style.recipe_title2}>Lobster Carpaccio</h5>
-              <ul className={style.recipe_genres}>Italia | Lobster | Finger Lime</ul>
-              <p className={style.recipe_summary}>
-                랍스터의 껍질을 제거한 뒤 오렌지, 다임, 소금, 후추를 넣고
-                진공한 뒤 수비드 56도씨에서 2시간 돌린다 비스큐소스, 오렌지주스,
-                화이트와인비...
-              </p>
-            </Link>
-          </div>
-
-          <div className={style.recipe}>
-            <Link to="/board/detail" >
-              <img className={style.recipe_img} src={img} alt="testimg" title="testimg" />
-              <h2 className={style.recipe_title}>랍스터 카르파치오</h2>
-              <h5 className={style.recipe_title2}>Lobster Carpaccio</h5>
-              <ul className={style.recipe_genres}>Italia | Lobster | Finger Lime</ul>
-              <p className={style.recipe_summary}>
-                랍스터의 껍질을 제거한 뒤 오렌지, 다임, 소금, 후추를 넣고
-                진공한 뒤 수비드 56도씨에서 2시간 돌린다 비스큐소스, 오렌지주스,
-                화이트와인비...
-              </p>
-            </Link>
-          </div>
+          {dataTest.map((data,index)=>(
+            <>{/* 레시피데이터 반복문 돌리면서 바인딩 */}
+              <div className={style.recipe}>
+                <Link to="/board/detail" onClick={()=>{
+                  window.sessionStorage.setItem(
+                    "detail_recipeId",data.recipeId
+                  );
+                }}>
+                  <img className={style.recipe_img} src={img} alt="testimg" title="testimg" />
+                  <h2 className={style.recipe_title}>여기는 제목임</h2>
+                  <h5 className={style.recipe_title2}>
+                    {data.keywordList.map((v)=>("#" + v.keywordName +"  "))}
+                  </h5>
+                  <ul className={style.recipe_genres}>
+                    {"Time " + data.cookingTime.toString() + "Min | level "
+                    +data.level + " | " +(data.isOpenable===1?"공개":"비공개")}
+                  </ul>
+                  <p className={style.recipe_summary}>
+                    {
+                    data.cookMethods.map((v)=>(" " + v.description +"  "))
+                    }
+                  </p>
+                </Link>
+              </div>
+            </>
+          ))}
         </div>
       </section>
     </div>
