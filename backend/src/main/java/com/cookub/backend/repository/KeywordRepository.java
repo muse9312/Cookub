e410d1bf06930dbeada718e442e7e204cf42093a
git config --global user.name JUN-GI <rlawns0528@naver.com>
2022-03-23 11:31:51 +0900

    ### fix : modify

--- backend/src/main/java/com/cookub/backend/repository/KeywordRepository.java
+++ backend/src/main/java/com/cookub/backend/repository/KeywordRepository.java
@@ -5,5 +5,5 @@ import com.cookub.backend.entity.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
-    
+    Keyword updateByKeyword(Keyword keyword);
 }
--- backend/src/main/java/com/cookub/backend/repository/RecipeRepository.java
+++ backend/src/main/java/com/cookub/backend/repository/RecipeRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecipeRepository extends JpaRepository<Recipe,Long> {
     List<Recipe> findByUser(User user);  // 유저 레시피 리스트 조회
-
+    Recipe updateByRecipes(Recipe recipe);
 //    List<Recipe> findById(Long recipeId); // 레시피 상세정보 조회
 
 }
