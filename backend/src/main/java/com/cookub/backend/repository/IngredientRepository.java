1aa6dde85ece3d1f4d2f9f8b4b3a242ba2911a8d
git config --global user.name JUN-GI <rlawns0528@naver.com>
2022-03-23 12:21:22 +0900

    ### fix : back error catch

--- backend/src/main/java/com/cookub/backend/repository/IngredientRepository.java
+++ backend/src/main/java/com/cookub/backend/repository/IngredientRepository.java
@@ -5,5 +5,5 @@ import com.cookub.backend.entity.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-    Ingredient updateByIngredient(Ingredient ingredient);
+    // Ingredient updateByIngredient(Ingredient ingredient);
 }
