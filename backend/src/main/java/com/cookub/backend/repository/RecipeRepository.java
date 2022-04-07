c49e0f0e34366c9fb588fd6b9b45493b6ece5de1
git config --global user.name JUN-GI <rlawns0528@naver.com>
2022-04-07 13:31:22 +0900

    ### fix : catch error insert Data

--- backend/src/main/java/com/cookub/backend/repository/RecipeRepository.java
+++ backend/src/main/java/com/cookub/backend/repository/RecipeRepository.java
@@ -13,4 +13,5 @@ public interface RecipeRepository extends JpaRepository<Recipe,Long> {
     List<Recipe> findByUser(User user);  // 유저 레시피 리스트 조회
     List<Recipe> findByisOpenable(Integer isOpenablelist);
 //    List<Recipe> findById(Long recipeId); // 레시피 상세정보 조회[]
+
 }
